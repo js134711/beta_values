96fa7fd551c992a3a5a30a067522bdd1cf6b832f
js134711 <jayasuriya2212@gmail.com>
2024-02-13 19:28:10 +0530

    slides_num_addtion

--- nanodrop_plots.R
+++ nanodrop_plots.R
@@ -19,7 +19,7 @@ plot(9:104,dna_qual$A260.A230)
 
 plot(dna_qual$A260.A230,dna_qual$A260.A280,col=c("green","red")[(1*(dna_qual$Nucleic.Acid.ng.uL.<60))+1])
 
-text(dna_qual$A260.A230,dna_qual$A260.A280,labels = as.character(9:104))
+text(dna_qual$A260.A230,dna_qual$A260.A280,labels = as.character(9:104),cex=.6)
 abline(h=c(1.85,1.88),lty=c(2,2),col="blue")
 abline(v=c(2.3,2.8),lty=c(2,2),col="red")
 abline(v=1.8,col="green",lty=2)
@@ -33,7 +33,66 @@ edge_18<-mean(1*(dna_qual$A260.A280<1.8&dna_qual$A260.A230<1.8&(row.names(dna_qu
 
 nonedge_78<-mean(1*(dna_qual$A260.A280<1.8&dna_qual$A260.A230<1.8&!(row.names(dna_qual)%in%edge_samples)))
 
-
 write.csv(dna_qual,"dna_qual.csv")
+
+devtools::install_github("https://github.com/omegahat/RDCOMClient.git")
+
+##################################
+###### dna isolation slides 
+library(DescTools)
+
+GetCurrPP()
+
+PpText("hello world", x = 100, y = 200, height = 50, width = 100, fontname = "Calibri", fontsize = 18,
+       bold = FALSE, italic = FALSE, col = "black", bg = "grey" ,
+       hasFrame = F, pp = DescToolsOptions("lastPP"))
+
+txt_v<-apply(dna_qual, 1,function(x){
+  
+  c<-paste0(x[2],"\n")
+  q1<-paste0(x[3],"\n")
+  q2<-paste0(x[4],"\n" )
+  paste0(c,q1,q2)
+} )
+
+lapply(txt_v[71:96], function(x){PpText(x, x = 1, y = 1, height = 50, width = 60, fontname = "Calibri", fontsize = 9,
+                                 bold = FALSE, italic = FALSE, col = "black", bg = "grey" ,
+                                 hasFrame = F, pp = DescToolsOptions("lastPP"))})
+
+
+###################################
+
+###confounding simulations 
+###loading dna methylation data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
